580ff71f51da9f0e177a1039f382a8761372fb87
0gawa <ogawa.daiki.infratop@gmail.com>
2025-09-10 01:01:28 +0900

    Backend: 完全な環境再構築

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -58,16 +58,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_08_103950) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
+    t.string "uid"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "name"
     t.string "nickname", null: false
     t.string "image"
@@ -81,7 +77,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_08_103950) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["smile_rank_id"], name: "index_users_on_smile_rank_id"
