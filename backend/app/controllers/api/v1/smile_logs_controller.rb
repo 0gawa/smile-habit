4b044cbb82b84680d2f7375ee1e1c6d9b756ac96
0gawa <ogawa.daiki.infratop@gmail.com>
2025-09-10 13:51:06 +0900

    SmileLogsControllerのcreateメソッドをリファクタリングし、smile_log_paramsメソッドを追加。CORS設定を整理し、Active Storageのマイグレーションを追加。

--- backend/app/controllers/api/v1/smile_logs_controller.rb
+++ backend/app/controllers/api/v1/smile_logs_controller.rb
@@ -2,11 +2,10 @@ class Api::V1::SmileLogsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # (AI分析はまだなので、一旦ダミーのスコアを入れる)
-    smile_log = current_user.smile_logs.build(
-      journal_entry: params[:journal_entry],
-      overall_score: rand(60..100) # 60点から100点のランダムなスコア
-    )
+    smile_log = current_user.smile_logs.build(smile_log_params)
+    
+    # AI分析はまだなので、一旦ダミーのスコアを入れる
+    smile_log.overall_score = rand(60..100)
 
     if smile_log.save
       render json: smile_log, status: :created
@@ -16,5 +15,11 @@ class Api::V1::SmileLogsController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def smile_log_params
+    params.permit(:photo, :journal_entry)
   end
 end
--- backend/app/models/smile_log.rb
+++ backend/app/models/smile_log.rb
@@ -1,3 +1,6 @@
 class SmileLog < ApplicationRecord
   belongs_to :user
+
+  # 投稿に画像は1枚まで
+  has_one_attached :photo
 end
