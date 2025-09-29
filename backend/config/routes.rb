523b359d7e922ce75bfd8ec6f8cbdf695353f6bf
0gawa <ogawa.daiki.infratop@gmail.com>
2025-09-30 01:11:11 +0900

    バックエンド側：順位の取得ができるアクションを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :smile_logs, only: [:create, :index, :show, :update]
+      resources :rankings, only: [:index]
       resource :mypage, only: [:show]
     end
   end
